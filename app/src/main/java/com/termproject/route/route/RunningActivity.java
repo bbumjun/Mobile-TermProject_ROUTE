4b6fb8a6be7cf398a8bda049c48817d9b1003f06
bbumjun <bumjun7928@naver.com>
2018-05-18 20:33:15 +0900

    commit

--- app/src/main/java/com/termproject/route/route/RunningActivity.java
+++ app/src/main/java/com/termproject/route/route/RunningActivity.java
@@ -17,14 +17,6 @@ Button mapBtn;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_running);
 
-        mapBtn =(Button)findViewById(R.id.mapButton);
-        mapBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),mapActivity.class);
-                startActivity(intent);
-            }
-        });
     }
     public boolean onTouchEvent(MotionEvent event) {
 
